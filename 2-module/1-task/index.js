21de579ac5081ffcf9a67d15340211ea335df90a
Irina Rogozhova <iro-extern@marketlogicsoftware.com>
2021-07-02 12:17:21 +0300

    2nd module tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  let hasNumberValues = false;
+
+  const valuesToIgnore = new Set([NaN, Infinity, -Infinity]);
+
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && !valuesToIgnore.has(salaries[key])) {
+      sum += salaries[key];
+      hasNumberValues = true;
+    }
+  }
+
+  return hasNumberValues ? sum : 0;
 }
